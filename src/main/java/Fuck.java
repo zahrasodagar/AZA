8d909a61c678eb7f280a9c5ae6fe0b893b4386c5
zahrasodagar <44804677zara>
2020-06-13 02:52:03 +0430

    test

--- src/main/java/Fuck.java
+++ src/main/java/Fuck.java
@@ -1,6 +1,6 @@
 public class Fuck {
     public static void main(String[] args){
         System.out.println("Hey");
-        System.out.println("Hey");
+        System.out.println("Hey Zahra");
     }
 }
