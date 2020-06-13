0ec45ae90c4849ebe02d4f96db2f9a91f21dbd25
lenovo <mapanahandeh1380@gmail.com>
2020-06-13 18:08:44 +0430

    Class Solve1 calculating the voltage of Nodes at a specific time

---
+++ src/main/java/Solve1.java
@@ -0,0 +1,62 @@
+
+public class Solve1 {
+    ////---------------   calculate voltage of nodes in t=T
+    public static void calculateVoltageAtT(){
+        int temp=0;
+        double Itotal1=0,Itotal2=0;
+        while (true){
+            ////-------------         reset all nodes
+            for (Object o: Main.everything){
+                if (o instanceof Nodes){
+                    Nodes.resetNodes();
+                }
+            }
+            for (Object o: Main.everything){
+                if (o instanceof Nodes){
+                    if(((Nodes) o).name.equals("0")||((Nodes) o).name.equals("gnd")){
+                        ((Nodes) o).visited=true;
+                    }
+                }
+            }
+            ////-------------         reset all nodes
+            ////-------------         visiting all nodes
+            for (Object o: Main.everything) {
+                if (o instanceof Nodes) {
+                    if (!((Nodes) o).visited) {
+                        Itotal1 = Nodes.getI();
+                        ((Nodes) o).v += Main.dV;
+                        Itotal2 = Nodes.getI();
+                        ((Nodes) o).v -= Main.dV;
+                        ((Nodes) o).v = ((Nodes) o).finalV + (Math.abs(Itotal1 - Itotal2)) * Main.dV / Main.dI;
+                        ((Nodes) o).visited = true;
+                    }
+                }
+            }
+            ////-------------         visiting all nodes
+            ////-------------     total input currents of all nodes is less than 0.01 so exit from the while
+            temp =0;
+            for (Object o: Main.everything){
+                if (o instanceof Nodes){
+                    if(Nodes.getI()>0.01||Nodes.getI()<-0.01){
+                        temp++;
+                    }
+                }
+            }
+            if(temp==0){
+                for (Object o: Main.everything){
+                    if (o instanceof Element){
+                        Element.update();
+                    }
+                }
+                for (Object o: Main.everything){
+                    if (o instanceof Nodes){
+                        Nodes.updateNodes();
+                    }
+                }
+                break;
+            }
+            ////-------------     total input currents of all nodes is less than 0.01 so exit from the while
+        }
+    }
+    ////---------------   calculate voltage of nodes in t=T
+}
