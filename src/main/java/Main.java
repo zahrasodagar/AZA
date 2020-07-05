a824bbfa300abdcc08cbb9578d3c4c9177f9c38e
zahrasodagar <61095718+zahrasodagar@users.noreply.github.com>
2020-07-05 16:13:02 +0430

    graphic (test)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,75 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.io.IOException;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.*;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import javafx.scene.control.Alert.AlertType;
+
+import java.awt.Label;
+import java.awt.Menu;
+import java.awt.MenuBar;
+import java.awt.MenuItem;
+import java.time.LocalDate;
+import java.awt.*;
 
 
-public class Main {
+
+public class Main extends Application {
     static String path=System.getProperty("user.dir")+"\\input.txt"; //default
+    Stage window;
+    Scene mainScene;
 
     public static void main(String[] args){
+        launch(args);
         Simulator.simulateFile();
     }
 
+    @Override
+    public void start(Stage stage) throws Exception {
+        window = stage;
+
+        MenuBar menuBar = new MenuBar();
+
+        Menu fileMenu = new Menu("File");
+        Menu editMenu = new Menu("Edit");
+        Menu helpMenu = new Menu("Help");
+
+        MenuItem newMenu = new MenuItem("New");
+        MenuItem openMenu = new MenuItem("Open");
+        MenuItem openRecentMenu = new MenuItem("Open Recent");
+        MenuItem saveMenu = new MenuItem("Save");
+        MenuItem reloadFileMenu = new MenuItem("Reload File");
+        MenuItem exitMenu = new MenuItem("Exit");
+
+        addMenuItems(fileMenu,newMenu,openMenu,openRecentMenu,saveMenu,reloadFileMenu,exitMenu);
+        addMenus(menuBar,fileMenu,editMenu,helpMenu);
+        VBox layout = new VBox(menuBar);
+        layout.getChildren().addAll(menuBar);
+        mainScene = new Scene(layout,800,600);
 
 
+        window.setScene(mainScene);
+        window.setTitle("input.txt Simulation");
+        window.show();
     }
+
+    public static void addMenuItems(Menu menu, MenuItem ... items){
+        for (MenuItem item: items){
+            menu.add(item);
+        }
+    }
+
+    public static void addMenus(MenuBar menuBar, Menu ... menus){
+        for (Menu menu: menus){
+            menuBar.add(menu);
+        }
+    }
+}
