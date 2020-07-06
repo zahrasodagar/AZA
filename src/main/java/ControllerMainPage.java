fcd6d9ebba20170c5abf8334e9f374abb4ae1d81
zahrasodagar <61095718+zahrasodagar@users.noreply.github.com>
2020-07-06 23:55:32 +0430

    mergish

---
+++ src/main/java/ControllerMainPage.java
@@ -0,0 +1,14 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.TextArea;
+
+import java.awt.*;
+
+public class ControllerMainPage {
+    @FXML public TextArea codeArea;
+
+
+
+}
