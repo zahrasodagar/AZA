e7f45b311ed1fbf83d88b5a2f4a130b15b002bd5
zahrasodagar <61095718+zahrasodagar@users.noreply.github.com>
2020-07-21 15:01:42 +0430

    diode fixed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -46,7 +46,7 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
-    static String path=System.getProperty("user.dir")+"\\input.txt"; //default
+    static String path="";//System.getProperty("user.dir")+"\\input.txt"; //default
     static String outputPath=System.getProperty("user.dir")+"\\output.txt"; //default
     static Stage window,w0;
     static Scene mainScene;
@@ -116,8 +116,6 @@ public class Main extends Application {
         a.setTitle(title);
         a.showAndWait();
     }
-
-
 
     EventHandler<MouseEvent> mouseHandler = new EventHandler<MouseEvent>() {
 
